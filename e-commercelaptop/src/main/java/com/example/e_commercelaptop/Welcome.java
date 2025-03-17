cf66f1fe1b8a5c487bc5d2e58218c8bbeb6e1aec
Ngo Hieu <ngominhhieu10g1@gmail.com>
2025-03-17 18:12:54 +0700

    Add them dependency vs cau hinh moi truong chay

--- e-commercelaptop/src/main/java/com/example/e_commercelaptop/Welcome.java
+++ e-commercelaptop/src/main/java/com/example/e_commercelaptop/Welcome.java
@@ -1,16 +1,40 @@
 package com.example.e_commercelaptop;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
 @RestController
+@Transactional
+@PropertySource("classpath:application.properties")
 public class Welcome {
 
     @GetMapping("/welcome")
     public String welcome(){
-        return "Hello Word";
+        return "Welcome Spring MVC";
     }
+    @GetMapping("/user")
+    public String getUser() {
+        return "Only User Can Access";
+    }
+    @GetMapping("/admin")
+    public String getAdmin() {
+        return "OnlyC Admin Can Access";
+    }
+   
+    
+    
+    
+    
+    
+    
     
 }
